440ef4a161bcc23f087f60af1fed34f9a13cc091
ibra4im <brahemansari514@gmail.com>
2026-01-06 14:00:33 +0100

    init

--- Easy/Two_Sum.c
+++ Easy/Two_Sum.c
@@ -12,10 +12,10 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
         int k = i + 1;
         while (k < numsSize)
         {
-            if(nums[i] + nums[j] == target)
+            if(nums[i] + nums[k] == target)
             {
                 arr_target[0] = i;
-                arr_target[1] = j;
+                arr_target[1] = k;
                 *returnSize = 2 ;
                 return arr_target;
             }
@@ -24,6 +24,6 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
         i++;
     }
 
-    *numsSize = 0;
+    *returnSize = 0;
     return NULL ;
 }
