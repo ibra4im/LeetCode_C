d139b4e0fe2b5367c88137fcc24ad73ccc1e0646
brahem <brahemansari514@gmail.com>
2026-04-10 12:00:59 +0100

    leet

---
+++ Easy/704BinarySearch.c
@@ -0,0 +1,13 @@
+int search(int* nums, int numsSize, int target) 
+{
+    int i = 0;
+    while (i < numsSize)
+    {
+        if (nums[i] == target)
+        {
+            return i;
+        }
+        i++;
+    }    
+    return -1;
+}
