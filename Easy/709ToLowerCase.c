2ed442876dabce7b1041d7eab6676c99ee9e8504
brahem <brahemansari514@gmail.com>
2026-04-11 14:37:28 +0100

    leet

---
+++ Easy/709ToLowerCase.c
@@ -0,0 +1,13 @@
+char* toLowerCase(char* s) 
+{
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+        {
+            s[i] += 32;
+        }
+        i++;
+    }    
+    return s;
+}
